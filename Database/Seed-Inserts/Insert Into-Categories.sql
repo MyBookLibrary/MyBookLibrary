793627cdde5fc3c92b4abb8b8d82c9f7285e182c
MikeSteiner <mike.steiner.77@gmail.com>
2017-05-15 16:22:26 +0300

    Add Database seed scripts

---
+++ Database/Seed-Inserts/Insert Into-Categories.sql
@@ -0,0 +1,14 @@
+USE [MyWoodenHouse]
+GO
+
+INSERT INTO [dbo].[Categories] ([Id] ,[Name])
+     VALUES(1,'House')
+INSERT INTO [dbo].[Categories] ([Id] ,[Name])
+     VALUES(2,'Bungalow')
+INSERT INTO [dbo].[Categories] ([Id] ,[Name])
+     VALUES(3,'Garage')
+INSERT INTO [dbo].[Categories] ([Id] ,[Name])
+     VALUES(4,'Cabin')
+GO
+
+
