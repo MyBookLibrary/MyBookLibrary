890f4eb140f4175e07fcd2c7100dd165a9baa0a6
MikeSteiner <mike.steiner.77@gmail.com>
2017-05-15 16:25:35 +0300

    Add Database seed script

---
+++ Database/Seed-Inserts/Insert Into-Genres.sql
@@ -0,0 +1,14 @@
+USE [BookLibraty]
+GO
+
+INSERT INTO [dbo].[Genres] ([Id] ,[Name])
+     VALUES(1,'Comedy')
+INSERT INTO [dbo].[Genres] ([Id] ,[Name])
+     VALUES(2,'Horror')
+INSERT INTO [dbo].[Genres] ([Id] ,[Name])
+     VALUES(3,'Biography')
+INSERT INTO [dbo].[Genres] ([Id] ,[Name])
+     VALUES(4,'Classics')
+GO
+
+
