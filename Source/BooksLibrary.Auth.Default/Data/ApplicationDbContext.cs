6652c01eb2e67739b02deda3e1198dba9a527def
MikeSteiner <mike.steiner.77@gmail.com>
2017-05-15 22:17:39 +0300

    Init the source folder with project working and Auth extracted in separate asembly.

---
+++ Source/BooksLibrary.Auth.Default/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BooksLibrary.Auth.Default.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksLibrary.Auth.Default.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
