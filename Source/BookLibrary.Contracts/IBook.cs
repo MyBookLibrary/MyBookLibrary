7c30b0af2a894313dc97b2cd9925d908c66102d7
MikeSteiner <mike.steiner.77@gmail.com>
2017-05-16 10:41:06 +0300

    Added new project with general contracts (Models)

---
+++ Source/BookLibrary.Contracts/IBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Contracts
+{
+    public interface IBook
+    {
+        string Title { get; set; }
+
+        string Description { get; set; }
+
+        IAuthor Author { get; set; }
+
+        IGenre Genre { get; set; }
+    }
+}
