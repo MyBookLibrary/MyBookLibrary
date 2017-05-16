5545e105b0e6128382b9d4e9b0657212b99205a7
MikeSteiner <mike.steiner.77@gmail.com>
2017-05-16 21:36:46 +0300

    Working on the BookController, BookMainViewModel etc.

---
+++ Source/BookLibrary.Clients.Web/ViewModels/AuthorMainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using BookLibrary.Constants.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookLibrary.ViewModels
+{
+    public class AuthorMainViewModel
+    {
+        [Required]
+        [Display(Name = "Id")]
+        [Range(Consts.Author.Id.MinValue, Consts.Author.Id.MaxValue, ErrorMessage = Consts.Author.Id.ErrorMessage)]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        [MaxLength(Consts.Author.FirstName.MaxLength, ErrorMessage = Consts.Author.FirstName.ErrorMessageMaxLength)]
+        [MinLength(Consts.Author.FirstName.MinLength, ErrorMessage = Consts.Author.FirstName.ErrorMessageMinLength)]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [MaxLength(Consts.Author.LastName.MaxLength, ErrorMessage = Consts.Author.LastName.ErrorMessageMaxLength)]
+        [MinLength(Consts.Author.LastName.MinLength, ErrorMessage = Consts.Author.LastName.ErrorMessageMinLength)]
+        public string LastName { get; set; }
+
+        [Display(Name = "Name")]
+        public string FullName
+        {
+            get
+            {
+                return this.FirstName + " " + this.LastName;
+            }
+        }
+    }
+}
---
+++ Source/BookLibrary.Clients.Web/ViewModels/BookMainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using BookLibrary.Constants.Models;
+using BookLibrary.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookLibrary.ViewModels
+{
+    public class BookMainViewModel
+    {
+        [Required]
+        [Display(Name = "Id")]
+        [Range(Consts.Book.Id.MinValue, Consts.Book.Id.MaxValue, ErrorMessage = Consts.Book.Id.ErrorMessage)]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Title")]
+        [MaxLength(Consts.Book.Title.MaxLength, ErrorMessage = Consts.Book.Title.ErrorMessageMaxLength)]
+        [MinLength(Consts.Book.Title.MinLength, ErrorMessage = Consts.Book.Title.ErrorMessageMinLength)]
+        public string Title { get; set; }
+
+        [Display(Name = "Description")]
+        [MaxLength(Consts.Book.Description.MaxLength, ErrorMessage = Consts.Book.Description.ErrorMessageMaxLength)]
+        [MinLength(Consts.Book.Description.MinLength, ErrorMessage = Consts.Book.Description.ErrorMessageMinLength)]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Creation Date")]
+        public DateTime CreationDate { get; set; }
+
+        //[Required]
+        //[Display(Name = "AuthorId")]
+        //public int AuthorId { get; set; }
+
+        //[Required]
+        //[Display(Name = "GenreId")]
+        //public int GenreId { get; set; }
+
+        [Display(Name = "Author")]
+        public AuthorMainViewModel Author { get; set; }
+
+        [Display(Name = "Genre")]
+        public GenreMainViewModel Genre { get; set; }
+
+    }
+}
