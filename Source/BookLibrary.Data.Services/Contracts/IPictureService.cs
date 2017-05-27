6319133d52f73cd92cc4d57804c45ad0cb858c8c
MikeSteiner <mike.steiner.77@gmail.com>
2017-05-27 11:54:31 +0300

    IPictureService interface added

---
+++ Source/BookLibrary.Data.Services/Contracts/IPictureService.cs
@@ -0,0 +1,26 @@
+﻿using BookLibrary.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Data.Services.Contracts
+{
+    public interface IPictureService
+    {
+        IEnumerable<IPictureModel> GetAllPictures();
+
+        IEnumerable<IPictureModel> GetAllPicturesSortedById();
+
+        IEnumerable<IPictureModel> GetAllPicturesSortedByFirstName();
+
+        IPictureModel GetPictureById(int? id);
+
+        int InsertPicture(IPictureModel pictureModel);
+
+        IPictureModel UpdatePicture(IPictureModel pictureModel);
+
+        void DeletePictureById(int? id);
+    }
+}
